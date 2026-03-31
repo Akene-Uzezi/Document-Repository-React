1e45a45cd79a36a10acfcba9b85b92c24ab90f2a
Akene Uzezi Elohoviano <uakene34@gmail.com>
2026-03-31 16:23:31 +0100

    added animation to the navbar component using the motion package

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,49 @@
-import image from "../../public/img/rhv logo.png";
+import image from "/img/rhv logo.png";
+import { motion } from "motion/react";
 import "../App.css";
+const variants = {
+  hidden: { opacity: 0, x: -20 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5, stagger: 10 } },
+};
+
+const childVariants = {
+  hidden: { opacity: 0, x: -10 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+};
 const Navbar = () => {
   return (
-    <div>
-      <nav className="bg-white border-b border-gray-200 w-full px-6 py-4">
-        <div className="max-w-7xl mx-auto flex items-center justify-between">
-          <a href="/" className="flex items-center space-x-3 group">
-            <img
+    <motion.div variants={variants} initial="hidden" animate="visible">
+      <motion.nav
+        variants={childVariants}
+        initial="hidden"
+        animate="visible"
+        className="bg-white border-b border-gray-200 w-full px-6 py-4"
+      >
+        <motion.div
+          variants={childVariants}
+          initial="hidden"
+          animate="visible"
+          className="max-w-7xl mx-auto flex items-center justify-between"
+        >
+          <motion.a href="/" className="flex items-center space-x-3 group">
+            <motion.img
+              variants={childVariants}
+              initial="hidden"
+              animate="visible"
               src={image}
               alt="Company Logo"
               className="h-9 w-auto object-contain block"
             />
 
-            <span className="text-xl font-bold text-slate-900 tracking-tight">
+            <motion.span
+              variants={childVariants}
+              initial="hidden"
+              animate="visible"
+              className="text-xl font-bold text-slate-900 tracking-tight"
+            >
               Document Repository
-            </span>
-          </a>
+            </motion.span>
+          </motion.a>
 
           {/* <div className="hidden md:flex items-center space-x-8">
             <a href="#" className="text-sm font-semibold text-blue-600">
@@ -67,9 +95,9 @@ const Navbar = () => {
               </svg>
             </button>
           </div> */}
-        </div>
-      </nav>
-    </div>
+        </motion.div>
+      </motion.nav>
+    </motion.div>
   );
 };
 
